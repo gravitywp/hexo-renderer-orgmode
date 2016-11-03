dbd1003cf9ec978f0e9483e9ae8153a62e62f762
guozhen <gravitywp@gmail.com>
2016-11-03 16:19:50 +0800

    make it runable

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 var renderer = require('./lib/renderer');
 var assign = require('object-assign');
 
-console.log("renderer", renderer);
 // Init option
 hexo.config.org = assign({
-  toc: false,
-  emacs: 'emacs',
-  common: '',
-  cachedir: './hexo-org-cache/'
 }, hexo.config.org);
 
 hexo.extend.renderer.register('org', 'html', renderer.bind(hexo), false);
 
-hexo.extend.filter.register('before_post_render', read_info);
+// hexo.extend.filter.register('before_post_render', read_info);
