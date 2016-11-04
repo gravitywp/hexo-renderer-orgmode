e5b8b327d52613b2c1e69e7f2de54d937a660d46
guozhen <gravitywp@gmail.com>
2016-11-04 13:37:51 +0800

    [fix] fix regex fro unordered list

--- index.js
+++ index.js
@@ -8,5 +8,4 @@ hexo.config.org = assign({
 }, hexo.config.org);
 
 hexo.extend.renderer.register('org', 'html', renderer.bind(hexo), false);
-
 hexo.extend.filter.register('before_post_render', read_info);
